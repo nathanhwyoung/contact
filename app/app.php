6302c2c2ebe4fff6683a7e987bc2023d230f7408
nhwilcox <student@epicodus.com>
2015-08-07 11:17:11 -0700

    app.php file mostly completed

--- app/app.php
+++ app/app.php
@@ -20,6 +20,10 @@
 
     $contact_app = new Silex\Application();
 
+    // debugging
+
+    $contact_app['debug'] = true;
+
     // makes the twig library available to the application and tells twig to
     // look for out template in the views folder
 
@@ -37,11 +41,25 @@
         return $contact_app['twig']->render('contacts.html.twig', array('contacts' => Contact::getAll()));
     });
 
-    $contact_app->post("/", function() use ($app) {
-        $new_contact = new Contact($_POST['add_name'], $_POST['add_number'], $_POST['add_address'])
+    // calls the post method on the $contact_app object and receives a URL path
+    // as its first argument, and a function that gives us access to the
+    // $contact_app variable. the method then creats a new contact object based
+    // on the date it receives from the form, and then saves (or pushes) the
+    // new object onto the $_SESSION array. then the method returns the
+    // $contact_app object (using twig) to call the render method (which
+    // receives a file path that contains the twig template and an array that
+    // contains the new contact object, which is added to the list)
+
+    $contact_app->post("/contacts", function() use ($contact_app) {
+        $new_contact = new Contact($_POST['add_name'], $_POST['add_number'], $_POST['add_address']);
         $new_contact->save();
+        return $contact_app['twig']->render('create_contact.html.twig', array('newcontact' => $new_contact));
     });
 
-    return $contact_app['twig']->render('contacts.html.twig');
+    $contact_app->post("/delete_contacts", function() use ($contact_app) {
+        Contact::deleteAll();
+    });
+
+    return $contact_app;
 
 ?>
